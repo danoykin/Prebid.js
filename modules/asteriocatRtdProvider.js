b5f6b501bfed26494fb726196b5ed75011481a52
Denis Anoykin <aden@asteriosoft.com>
2024-09-09 01:43:12 +0200

    Asteriocat RTD module

--- modules/asteriocatRtdProvider.js
+++ modules/asteriocatRtdProvider.js
@@ -1,81 +1,52 @@
 import {submodule} from '../src/hook.js';
 import {ajax} from '../src/ajax.js';
-import {isPlainObject, logInfo, mergeDeep} from '../src/utils.js';
+import {deepSetValue, prefixLog} from '../src/utils.js';
 import {getStorageManager} from '../src/storageManager.js';
 import {MODULE_TYPE_RTD} from '../src/activities/modules.js';
+import {getRefererInfo} from '../src/refererDetection.js'
 
 const MODULE_NAME = 'realTimeData';
 const SUBMODULE_NAME = 'asteriocatRtdProvider';
-const DEFAULT_ENDPOINT = 'https://asteriocat.com';
+const DEFAULT_ENDPOINT = 'https://endpt.asteriocat.com';
+
+const {logInfo} = prefixLog('|AsterioCatRtdProvider|')
 
 export const storage = getStorageManager({moduleType: MODULE_TYPE_RTD, moduleName: SUBMODULE_NAME});
 
-/**
- * Add real-time data & merge segments.
- * @param {Object} ortb2
- * @param {Object} rtd
- */
-export function addRealTimeData(ortb2, rtd) {
-  if (isPlainObject(rtd.ortb2)) {
-    logInfo('Merge Asteriocat data to BidRequest');
-    mergeDeep(ortb2, rtd.ortb2);
-  }
-}
-
-function getCategoryData(endpoint, callback) {
-  let pageUrl = window.location.href;
-  let storageData;
-  try {
-    storageData = storage.getDataFromLocalStorage(pageUrl);
-  } catch (e) {
-  }
-  if (storageData) {
-    const json = JSON.parse(storageData);
-    if (json?.length > 0) {
-      logInfo('Asteriocat data received from storage:', storageData);
-      callback(json);
-      return;
-    }
-  }
-  const url = `${endpoint || DEFAULT_ENDPOINT}?pageUrl=${encodeURIComponent(pageUrl)}`;
-  ajax(url, {
-    success: (response) => {
-      let json = JSON.parse(response);
-      if (json?.status === 'ok' && json.categories?.length > 0) {
-        storage.setDataInLocalStorage(pageUrl, JSON.stringify(json.categories));
-        callback(json.categories);
-      }
-    },
-    error: () => {
-      callback(null);
-    }
-  }, null);
-}
+let pageCategories
+let pageCategoriesPromise
+let pageCategoriesTime
 
 export function getRealTimeData(reqBidsConfigObj, onDone, rtdConfig) {
-  let endpoint;
-  if (rtdConfig && isPlainObject(rtdConfig.params)) {
-    endpoint = rtdConfig.params.endpoint;
-  }
   let siteConfig = reqBidsConfigObj.ortb2Fragments?.global?.site;
-  if (['cat', 'pagecat', 'sectioncat'].some(key => siteConfig?.[key]?.length > 0)) {
-    logInfo('Asteriocat not called, BidRequest already contains categories');
+  if (['cat', 'sectioncat', 'pagecat'].some(key => siteConfig?.[key]?.length > 0)) {
+    logInfo('request has already contained categories');
     onDone();
     return;
   }
-  getCategoryData(endpoint, (categoryData) => {
-    if (categoryData) {
-      const data = {
-        ortb2: {
-          site: {
-            cat: categoryData
-          }
-        }
-      }
-      addRealTimeData(reqBidsConfigObj.ortb2Fragments?.global, data);
-    }
-    onDone();
-  });
+
+  if (pageCategories === undefined) {
+    const start = Date.now()
+    pageCategoriesPromise.then(() => {
+      logInfo('Waited: ' + (Date.now() - start) + ' ms')
+      injectCategories(reqBidsConfigObj, onDone)
+    }) // wait completion of call to server
+  } else {
+    injectCategories(reqBidsConfigObj, onDone)
+  }
+}
+
+function injectCategories(reqBidsConfigObj, onDone) {
+  const siteConfig = reqBidsConfigObj.ortb2Fragments?.global.site;
+
+  deepSetValue(siteConfig, 'ext.asteriocat.enabled', true);
+  deepSetValue(siteConfig, 'ext.asteriocat.time', pageCategoriesTime);
+
+  if (pageCategories && pageCategories.length > 0) {
+    deepSetValue(siteConfig, 'pagecat', pageCategories)
+  }
+
+  onDone()
 }
 
 /**
@@ -84,8 +55,35 @@ export function getRealTimeData(reqBidsConfigObj, onDone, rtdConfig) {
  * @param {Object} userConsent
  * @return {boolean}
  */
-function init(provider, userConsent) {
-  return true;
+export function init(provider, userConsent) {
+  return true
+}
+
+function readPageCategories() {
+  return new Promise((resolve, reject) => {
+    let endpoint = window.asteriocatEndpoint || DEFAULT_ENDPOINT;
+
+    const refererInfo = getRefererInfo()
+    let pageUrl = refererInfo.page
+
+    const url = `${endpoint}/api/categorize?url=${encodeURIComponent(pageUrl)}`;
+    ajax(url, {
+      success: (response) => {
+        let categories = ''
+        let json = JSON.parse(response);
+        if (json?.status === 'ok' && json.categories?.length > 0) {
+          categories = json.categories.join(',')
+          logInfo('Categories: ', json.categories)
+        } else {
+          logInfo('No categories, status: ', json?.status)
+        }
+        resolve(categories)
+      },
+      error: () => {
+        resolve('')
+      }
+    }, null);
+  });
 }
 
 export const asteriocatSubmodule = {
@@ -94,4 +92,13 @@ export const asteriocatSubmodule = {
   init: init
 };
 
-submodule(MODULE_NAME, asteriocatSubmodule);
+function beforeInit() {
+  const start = Date.now()
+  pageCategoriesPromise = readPageCategories().then(categories => {
+    pageCategories = categories
+    pageCategoriesTime = Date.now() - start
+  })
+  submodule(MODULE_NAME, asteriocatSubmodule)
+}
+
+beforeInit()
